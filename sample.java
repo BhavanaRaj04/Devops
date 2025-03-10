b3e1e795263838969ffab4eb92dd28906ba3f30b
BhavanaRaj04 <166927881+BhavanaRaj04@users.noreply.github.com>
2025-03-10 09:54:43 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,14 @@
+package hai;
+public class sample {
+
+	public static void main(String[] args) {
+		int a=23;
+		if(a%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+			System.out.println("odd");
+	}
+
+}
